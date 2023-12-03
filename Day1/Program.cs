c6253b9887b6625de5143d5cc500485ef9cd03e8
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-12-03 19:00:49 +0100

    add solution for part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -15,8 +15,7 @@ foreach (var i in inputArr)
 {
     //check the string of it contains numbers and store them in een collection
     MatchCollection m = re.Matches(i);
-    var number = int.Parse(m[0].ToString() + m[m.Count - 1]);//creating the two digit number 
-    Console.WriteLine(number);
+    var number = int.Parse(m[0].ToString() + m[m.Count - 1]);//creating the two digit number
     total += number;
 }
 
