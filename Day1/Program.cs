daa8d850d5331a9c4139369b4d1dead6ad6594f2
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2023-12-03 14:49:20 +0100

    solution for day 1 part 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+
+
+//string[] inputArr = {"1abc2","pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"};
+string[] inputArr = File.ReadAllLines(@"C:\Users\yanick\RiderProjects\AdventOfCode2023\Day1\input.txt");
+int total = 0;
+
+//get the positive numbers out of a string
+Regex re = new Regex(@"\d");
+
+//loop over the input array
+foreach (var i in inputArr)
+{
+    //check the string of it contains numbers and store them in een collection
+    MatchCollection m = re.Matches(i);
+    var number = int.Parse(m[0].ToString() + m[m.Count - 1]);//creating the two digit number 
+    Console.WriteLine(number);
+    total += number;
+}
+
+Console.WriteLine(total);
+
+
